cf66ee34d3a649cf1d728bfb35eacb48612590ff
dcontrerasdrz <dtcontreras@drz.global>
2023-02-14 21:02:49 -0300

    v1

--- aula 3/js/script.js
+++ aula 3/js/script.js
@@ -1,16 +1,10 @@
 const posts = [
     {
         title: "Google Chrome",
+        subtitle:'History',
         contend: "google is a meu new best friend para developed by david or me :)"   
     },
-    {
-        title: "Mozilla Firefox",
-        contend:"Mozilla is a meu new best friend para developed by david or me :)" 
-    },
-    {
-        title: "Microsoft Edge",
-        contend:"Microsoft Edge is a meu new best friend para developed by david or me :)"
-    },
+   
 ];
 
 const wrapper = document.getElementById('posts');
@@ -20,12 +14,20 @@ for (var i = 0; i < posts.length; i++){
     const article = document.createElement("article");
     article.classList.add("article");
 
-    const title = document.createElement("h2");
+    const title = document.createElement("h1");
     title.classList.add("article-title");
 
     const titleText = document.createTextNode(postData.title);
     title.appendChild(titleText);
     article.appendChild(title);
+
+    const subtitle = document.createElement("h3");
+    article.classList.add("article-subtitle");
+
+    const subtitleTex = document.createTextNode(postData.subtitle);
+    subtitle.appendChild(subtitleTex);
+    title.appendChild(subtitle);
+
     
     
     const contend = document.createElement("p");
